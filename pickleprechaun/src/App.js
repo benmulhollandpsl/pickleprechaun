26d2ee0e93aff03ccd6439005e66635b04de7e9d
Ben Mulholland <ben.mulholland@gmail.com>
2021-10-22 16:53:46 -0400

    nav header main footer and css started in appjs gradients and shadow next

--- pickleprechaun/src/App.js
+++ pickleprechaun/src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
 import TacoFriendsGallery from './data/TacoFriendsGallery.gif'
 import pickleprechaunlogobig from './data/pickleprechaunlogobig.png'
 import './App.css';
 
+
+//Nav
+function Nav(props) {
+  return(
+  <section>
+  
+  <li>Pickleprechaun</li>
+  <li>about</li>
+  <li>contact</li>
+  
+  </section>
+  )
+}
+
+// Header
 function Header(props) {
   console.log(props)
   return(
     <section>
-      <h1></h1>
+    
       <img src ={pickleprechaunlogobig}
       height ={900}
       alt="logo for Pickleprechaun studio"/>
@@ -15,27 +30,66 @@ function Header(props) {
   )
 }
 
+// Main or body
+function Main(props) {
+  console.log(props) 
+  return( 
+  <section>
+    <img src = {TacoFriendsGallery}
+    height = {600}
+    alt="Taco Friends NFT gif"/>
+    <p>cards with images here next</p>
+  </section>
+
+
+
+  )
+}
+
+
+// Footer
+function Footer(props) {
+  console.log(props)
+  return(
+    <section>
+      <p>Copyright {props.year}</p>
+    </section>
+
+
+  )
+}
+
+
+
 function App() {
   return (
     <div className="App">
+      
+      
+      <nav className="App-nav">
+        <Nav />
+        </nav>
+    
       <header className="App-header">
         <Header/>
         
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        
-        </p>
-        <p>Pickleprechaun coming soon</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <p>scroll down for newest stuff</p>
+       
       </header>
+
+
+      <main className="App-main">
+        <Main />
+      </main>
+
+      
+      <footer className="App-footer">
+        <Footer year={new Date().getFullYear()}/>
+      </footer>
+
+
+
     </div>
   );
 }
